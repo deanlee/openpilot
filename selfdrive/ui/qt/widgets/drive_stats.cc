db60d7aa7df2f3c018a191ae64b45b4c1ca20d55
deanlee <deanlee3@gmail.com>
2021-06-18 01:02:23 +0800

    dd

--- selfdrive/ui/qt/widgets/drive_stats.cc
+++ selfdrive/ui/qt/widgets/drive_stats.cc
@@ -11,39 +11,37 @@
 
 const double MILE_TO_KM = 1.60934;
 
-namespace {
-
-QLabel* newLabel(const QString& text, bool unitLabel = true, QDataWidgetMapper* mapper = nullptr) {
-  QLabel* label = new QLabel(text);
-  label->setStyleSheet(unitLabel ? "font-size: 45px; font-weight: 500;" : "font-size: 80px; font-weight: 600;");
-  if (mapper) mapper->addMapping(label, mapper->currentIndex());
-  return label;
-}
-
-}  // namespace
-
 DriveStats::DriveStats(QWidget* parent) : QWidget(parent) {
   metric_ = Params().getBool("IsMetric");
   QGridLayout* main_layout = new QGridLayout(this);
 
   QDataWidgetMapper* mapper = new QDataWidgetMapper(this);
-  model = new QStandardItemModel(0, 8, this);
+  model = new QStandardItemModel(0, 12, this);
   mapper->setModel(model);
-  for (auto time : {"ALL TIME", "PAST WEEK"}) {
+
+  auto newLable = [=](const QString& text, bool unitLabel) -> QLabel* {
+    QLabel* label = new QLabel(text);
+    qInfo() << mapper->currentIndex();
+    mapper->addMapping(label, mapper->currentIndex());
+    label->setStyleSheet(unitLabel ? "font-size: 45px; font-weight: 500;" : "font-size: 80px; font-weight: 600;");
+    return label;
+  };
+
+  auto add_stats_layouts = [=](const QString &title) {
     int row = main_layout->rowCount();
+    main_layout->addWidget(new QLabel(title), row++, 0, 1, 3);
 
-    QLabel* title = new QLabel(time);
-    main_layout->addWidget(title, row++, 0, 1, 3);
-    title->setStyleSheet("font-size: 48px; font-weight: 500;");
+    main_layout->addWidget(newLable("0", false), row, 0, Qt::AlignLeft);
+    main_layout->addWidget(newLable("0", false), row, 1, Qt::AlignLeft);
+    main_layout->addWidget(newLable("0", false), row, 2, Qt::AlignLeft);
 
-    main_layout->addWidget(newLabel("0", false, mapper), row, 0, Qt::AlignLeft);
-    main_layout->addWidget(newLabel("0", false, mapper), row, 1, Qt::AlignLeft);
-    main_layout->addWidget(newLabel("0", false, mapper), row, 2, Qt::AlignLeft);
+    main_layout->addWidget(newLable("DRIVES", true), row + 1, 0, Qt::AlignLeft);
+    main_layout->addWidget(newLable(getDistanceUnit(), true), row + 1, 1, Qt::AlignLeft);
+    main_layout->addWidget(newLable("HOURS", true), row + 1, 2, Qt::AlignLeft);
+  };
 
-    main_layout->addWidget(newLabel("DRIVES"), row + 1, 0, Qt::AlignLeft);
-    main_layout->addWidget(newLabel(getDistanceUnit(), true, mapper), row + 1, 1, Qt::AlignLeft);
-    main_layout->addWidget(newLabel("HOURS"), row + 1, 2, Qt::AlignLeft);
-  }
+  add_stats_layouts("ALL TIME");
+  add_stats_layouts("PAST WEEK");
   mapper->toFirst();
 
   std::string dongle_id = Params().get("DongleId");
@@ -63,7 +61,8 @@ void DriveStats::updateStats() {
     model->setData(model->index(0, i * 2), (int)(j["routes"].toDouble()));
     model->setData(model->index(0, i * 2 + 1), (int)j["distance"].toDouble() * (metric_ ? MILE_TO_KM : 1));
     model->setData(model->index(0, i * 2 + 2), (int)(j["minutes"].toDouble() / 60));
-    model->setData(model->index(0, i * 2 + 3), getDistanceUnit());
+    qInfo() << "distance " << getDistanceUnit();
+    model->setData(model->index(0, i * 2 + 4), getDistanceUnit());
   }
 }
 
@@ -73,10 +72,8 @@ void DriveStats::parseResponse(const QString& response) {
     qDebug() << "JSON Parse failed on getting past drives statistics";
     return;
   }
-  if (stats_ != doc) {
-    stats_ = doc;
-    updateStats();
-  }
+  stats_ = doc;
+  updateStats();
 }
 
 void DriveStats::showEvent(QShowEvent* event) {
