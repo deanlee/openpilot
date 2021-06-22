9daf87ab12b8c6636e37bd58bd4437a0da06e411
deanlee <deanlee3@gmail.com>
2021-06-22 12:14:57 +0800

    dd

--- selfdrive/ui/qt/widgets/scrollview.h
+++ selfdrive/ui/qt/widgets/scrollview.h
@@ -1,12 +1,21 @@
 #pragma once
 
 #include <QScrollArea>
-
-class ScrollView : public QScrollArea {
+#include <QGraphicsView>
+#include <QResizeEvent>
+#include <QGraphicsProxyWidget>
+#include <QGraphicsScene>
+#include <QOpenGLWidget>
+#include <QResizeEvent>
+#include <QPixmapCache>
+class ScrollView : public QGraphicsView {
   Q_OBJECT
 
 public:
   explicit ScrollView(QWidget *w = nullptr, QWidget *parent = nullptr);
 protected:
   void hideEvent(QHideEvent *e) override;
+  void resizeEvent(QResizeEvent *event) override;
+  QGraphicsProxyWidget *proxyWidget;
+  QWidget *w_;
 };
