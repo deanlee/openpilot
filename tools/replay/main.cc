dfe3d0ee132847c1a054ccb4ab6b156bbe27da0d
deanlee <deanlee3@gmail.com>
2022-08-29 13:38:05 +0800

    dd

--- tools/replay/main.cc
+++ tools/replay/main.cc
@@ -6,26 +6,6 @@
 #include "tools/replay/replay.h"
 
 const QString DEMO_ROUTE = "4cf7a6ad03080c90|2021-09-29--13-46-36";
-
-
-// void replayMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
-//   if (message_handler) {
-//     message_handler(type, msg_buf);
-//   } else {
-//     if (type == ReplyMsgType::Debug) {
-//       std::cout << "\033[38;5;248m" << msg_buf << "\033[00m" << std::endl;
-//     } else if (type == ReplyMsgType::Warning) {
-//       std::cout << "\033[38;5;227m" << msg_buf << "\033[00m" << std::endl;
-//     } else if (type == ReplyMsgType::Critical) {
-//       std::cout << "\033[38;5;196m" << msg_buf << "\033[00m" << std::endl;
-//     } else {
-//       std::cout << msg_buf << std::endl;
-//     }
-//   }
-
-//   free(msg_buf);
-// }
-
 std::unique_ptr<ConsoleUI> console_ui;
 
 int main(int argc, char *argv[]) {
