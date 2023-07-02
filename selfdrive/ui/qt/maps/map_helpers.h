eb8504d0d76121ce32c73f4a94cc9a4937972d18
deanlee <deanlee3@gmail.com>
2023-07-03 02:20:36 +0800

    dd

--- selfdrive/ui/qt/maps/map_helpers.h
+++ selfdrive/ui/qt/maps/map_helpers.h
@@ -14,8 +14,6 @@ const QString MAPBOX_TOKEN = util::getenv("MAPBOX_TOKEN").c_str();
 const QString MAPS_HOST = util::getenv("MAPS_HOST", MAPBOX_TOKEN.isEmpty() ? "https://maps.comma.ai" : "https://api.mapbox.com").c_str();
 const QString MAPS_CACHE_PATH = "/data/mbgl-cache-navd.db";
 
-
-
 QString get_mapbox_token();
 QMapboxGLSettings get_mapbox_settings();
 QGeoCoordinate to_QGeoCoordinate(const QMapbox::Coordinate &in);
