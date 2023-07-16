1efb7fa434aa1f59b6b2e465b7f7ddd601e88575
deanlee <deanlee3@gmail.com>
2023-07-16 20:50:11 +0800

    dd

--- system/loggerd/loggerd.cc
+++ system/loggerd/loggerd.cc
@@ -1,3 +1,4 @@
+#include <deque>
 #include <unordered_map>
 
 #include "system/loggerd/encoder/encoder.h"
@@ -54,13 +55,13 @@ void rotate_if_needed(LoggerdState *s) {
 
 struct RemoteEncoder {
   RemoteEncoder(const EncoderInfo &encoder_info) : encoder_info(encoder_info) {}
-  size_t write_encode_data(LoggerState *logger, const cereal::Event::Reader &event);
-  size_t write_video(const cereal::EncodeData::Reader &edata, const cereal::EncodeIndex::Reader &idx);
+  size_t write_encode_data(LoggerdState *s, const cereal::Event::Reader &event);
+  size_t write_video(LoggerdState *s, const cereal::EncodeData::Reader &edata, const cereal::EncodeIndex::Reader &idx);
 
   std::unique_ptr<VideoWriter> video_writer;
   int encoderd_segment_offset;
   int current_segment = -1;
-  std::vector<Message *> q;
+  std::deque<Message *> q;
   int dropped_frames = 0;
   bool recording = false;
   bool marked_ready_to_rotate = false;
@@ -68,13 +69,13 @@ struct RemoteEncoder {
   const EncoderInfo encoder_info;
 };
 
-size_t RemoteEncoder::write_encode_data(LoggerState *logger, const cereal::Event::Reader &event) {
+size_t RemoteEncoder::write_encode_data(LoggerdState *s, const cereal::Event::Reader &event) {
   auto edata = (event.*(encoder_info.get_encode_data_func))();
   const auto idx = edata.getIdx();
 
   // write video
   if (encoder_info.record) {
-    write_video(edata, idx);
+    write_video(s, edata, idx);
   }
 
   // put it in log stream as the idx packet
@@ -83,11 +84,11 @@ size_t RemoteEncoder::write_encode_data(LoggerState *logger, const cereal::Event
   evt.setLogMonoTime(event.getLogMonoTime());
   (evt.*(encoder_info.set_encode_idx_func))(idx);
   auto bytes = msg.toBytes();
-  logger_log(logger, (uint8_t *)bytes.begin(), bytes.size(), true);  // always in qlog?
+  logger_log(&s->logger, (uint8_t *)bytes.begin(), bytes.size(), true);  // always in qlog?
   return bytes.size();
 }
 
-size_t RemoteEncoder::write_video(const cereal::EncodeData::Reader &edata, const cereal::EncodeIndex::Reader &idx) {
+size_t RemoteEncoder::write_video(LoggerdState *s, const cereal::EncodeData::Reader &edata, const cereal::EncodeIndex::Reader &idx) {
   size_t written = 0;
   const bool is_key_frame = idx.getFlags() & V4L2_BUF_FLAG_KEYFRAME;
 
@@ -98,7 +99,7 @@ size_t RemoteEncoder::write_video(const cereal::EncodeData::Reader &edata, const
         LOGW("%s: dropped %d non iframe packets before init", encoder_info.publish_name, dropped_frames);
         dropped_frames = 0;
       }
-      video_writer.reset(new VideoWriter(segment_path.c_str(), encoder_info.filename, idx.getType() != cereal::EncodeIndex::Type::FULL_H_E_V_C,
+      video_writer.reset(new VideoWriter(s->segment_path, encoder_info.filename, idx.getType() != cereal::EncodeIndex::Type::FULL_H_E_V_C,
                                          encoder_info.frame_width, encoder_info.frame_height, encoder_info.fps, idx.getType()));
       auto header = edata.getHeader();
       video_writer->write((uint8_t *)header.begin(), header.size(), idx.getTimestampEof() / 1000, true, false);
@@ -126,7 +127,6 @@ int write_encoder_msg(RemoteEncoder &re, LoggerdState *s, Message *msg) {
   auto event = cmsg.getRoot<cereal::Event>();
   auto edata = (event.*(re.encoder_info.get_encode_data_func))();
   auto idx = edata.getIdx();
-  auto flags = idx.getFlags();
 
   // encoderd can have started long before loggerd
   if (!re.seen_first_packet) {
@@ -142,66 +142,19 @@ int write_encoder_msg(RemoteEncoder &re, LoggerdState *s, Message *msg) {
     // if this is a new segment, we close any possible old segments, move to the new, and process any queued packets
     if (re.current_segment != s->rotate_segment) {
       if (re.recording) {
-        re.writer.reset();
+        re.video_writer.reset();
         re.recording = false;
       }
       re.current_segment = s->rotate_segment;
       re.marked_ready_to_rotate = false;
       // we are in this segment now, process any queued messages before this one
-      if (!re.q.empty()) {
-        for (auto &qmsg: re.q) {
-          bytes_count += write_encoder_msg(re, s, qmsg);
-        }
-        re.q.clear();
+      for (; !re.q.empty(); re.q.pop_front()) {
+        auto &qmsg = re.q.front();
+        capnp::FlatArrayMessageReader msg_reader({(capnp::word *)qmsg->getData(), qmsg->getSize() / sizeof(capnp::word)});
+        bytes_count += re.write_encode_data(s, msg_reader.getRoot<cereal::Event>());
       }
     }
-
-    // if we aren't recording yet, try to start, since we are in the correct segment
-    if (!re.recording) {
-      if (flags & V4L2_BUF_FLAG_KEYFRAME) {
-        // only create on iframe
-        if (re.dropped_frames) {
-          // this should only happen for the first segment, maybe
-          LOGW("%s: dropped %d non iframe packets before init", re.encoder_info.publish_name, re.dropped_frames);
-          re.dropped_frames = 0;
-        }
-        // if we aren't actually recording, don't create the writer
-        if (re.encoder_info.record) {
-          re.writer.reset(new VideoWriter(s->segment_path,
-            re.encoder_info.filename, idx.getType() != cereal::EncodeIndex::Type::FULL_H_E_V_C,
-            re.encoder_info.frame_width, re.encoder_info.frame_height, re.encoder_info.fps, idx.getType()));
-          // write the header
-          auto header = edata.getHeader();
-          re.writer->write((uint8_t *)header.begin(), header.size(), idx.getTimestampEof()/1000, true, false);
-        }
-        re.recording = true;
-      } else {
-        // this is a sad case when we aren't recording, but don't have an iframe
-        // nothing we can do but drop the frame
-        delete msg;
-        ++re.dropped_frames;
-        return bytes_count;
-      }
-    }
-
-    // we have to be recording if we are here
-    assert(re.recording);
-
-    // if we are actually writing the video file, do so
-    if (re.writer) {
-      auto data = edata.getData();
-      re.writer->write((uint8_t *)data.begin(), data.size(), idx.getTimestampEof()/1000, false, flags & V4L2_BUF_FLAG_KEYFRAME);
-    }
-
-    // put it in log stream as the idx packet
-    MessageBuilder bmsg;
-    auto evt = bmsg.initEvent(event.getValid());
-    evt.setLogMonoTime(event.getLogMonoTime());
-    (evt.*(re.encoder_info.set_encode_idx_func))(idx);
-    auto new_msg = bmsg.toBytes();
-    logger_log(&s->logger, (uint8_t *)new_msg.begin(), new_msg.size(), true);   // always in qlog?
-    bytes_count += new_msg.size();
-
+    bytes_count += re.write_encode_data(s, event);
     // free the message, we used it
     delete msg;
   } else if (offset_segment_num > s->rotate_segment) {
