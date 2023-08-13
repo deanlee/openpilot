3148ce80a171386e209006c7f2cd8f7372e45538
deanlee <deanlee3@gmail.com>
2023-08-13 16:02:30 +0800

    dd

--- common/util.cc
+++ common/util.cc
@@ -272,34 +272,26 @@ std::string sprintf(const char* format, ...) {
 }
 
 std::string vsprintf(const char* format, va_list ap) {
-  std::string result;
+  auto format_str = [](char *buf, size_t size, const char* format, va_list ap) -> int {
+    va_list ap_copy;
+    va_copy(ap_copy, ap);
+    int ret = ::vsnprintf(buf, size, format, ap_copy);
+    va_end(ap_copy);
+    return ret;
+  };
 
+  std::string result;
   // First try with a small fixed size buffer.
   char stack_buf[1024];
-  va_list ap_copy;
-  va_copy(ap_copy, ap);
-  int ret = ::vsnprintf(stack_buf, std::size(stack_buf), format, ap_copy);
-  va_end(ap_copy);
-
+  int ret = format_str(stack_buf, std::size(stack_buf), format, ap);
   if (ret >= 0) {
     if (static_cast<size_t>(ret) < std::size(stack_buf)) {
-      // It fit.
       result.append(stack_buf, ret);
     } else {
-      // We need exactly "ret + 1" characters.
-      size_t mem_length = static_cast<size_t>(ret) + 1;
-      std::vector<char> mem_buf(mem_length);
-
-      va_copy(ap_copy, ap);
-      ret = ::vsnprintf(&mem_buf[0], mem_length, format, ap_copy);
-      va_end(ap_copy);
-
-      if (ret >= 0 && static_cast<size_t>(ret) < mem_length) {
-        result.append(&mem_buf[0], ret);
-      }
+      result.resize(ret);
+      format_str(&result[0], ret + 1, format, ap);
     }
   }
-
   return result;
 }
 
