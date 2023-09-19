eb8f5a684acfe9571be29a07202ccaafbbae2205
deanlee <deanlee3@gmail.com>
2023-09-20 00:35:38 +0800

    rename rangeChanged to zoomChanged

--- tools/cabana/videowidget.h
+++ tools/cabana/videowidget.h
@@ -68,7 +68,7 @@ class VideoWidget : public QFrame {
 
 public:
   VideoWidget(QWidget *parnet = nullptr);
-  void updateTimeRange(double min, double max, bool is_zommed);
+  void zoomChanged(double min, double max, bool is_zommed);
   void setMaximumTime(double sec);
 
 protected:
