ee7e8f1daa4551279a8b140e5e5f82683072ebef
deanlee <deanlee3@gmail.com>
2023-11-10 23:03:31 +0800

    dd

--- tools/replay/replay.cc
+++ tools/replay/replay.cc
@@ -243,7 +243,7 @@ void Replay::queueSegment() {
   auto begin = std::prev(cur, std::min<int>(segment_cache_limit / 2, std::distance(segments_.begin(), cur)));
   auto end = std::next(begin, std::min<int>(segment_cache_limit, segments_.size()));
   // load one segment at a time
-  auto it = std::find_if(cur, end, [](auto &it) { return (it.second && !it.second->isLoaded()) || !it.second; });
+  auto it = std::find_if(cur, end, [](auto &it) { return !it.second || !it.second->isLoaded(); });
   if (it != end && !it->second) {
     rDebug("loading segment %d...", it->first);
     it->second = std::make_unique<Segment>(it->first, route_->at(it->first), flags_, allow_list);
