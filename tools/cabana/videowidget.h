b688811645e26445c05775fd1bba031406d2a9fb
deanlee <deanlee3@gmail.com>
2024-07-15 23:42:06 +0800

    refactor

--- tools/cabana/videowidget.h
+++ tools/cabana/videowidget.h
@@ -2,16 +2,13 @@
 
 #include <map>
 #include <memory>
-#include <optional>
 #include <set>
 #include <string>
-#include <utility>
 
 #include <QHBoxLayout>
 #include <QFrame>
 #include <QPropertyAnimation>
 #include <QSlider>
-#include <QTabBar>
 
 #include "selfdrive/ui/qt/widgets/cameraview.h"
 #include "tools/cabana/utils/util.h"
@@ -27,7 +24,6 @@ class InfoLabel : public QWidget {
 public:
   InfoLabel(QWidget *parent);
   void showPixmap(const QPoint &pt, const QString &sec, const QPixmap &pm, const AlertInfo &alert);
-  void showAlert(const AlertInfo &alert);
   void paintEvent(QPaintEvent *event) override;
   QPixmap pixmap;
   QString second;
@@ -43,7 +39,6 @@ public:
   void setCurrentSecond(double sec) { setValue(sec * factor); }
   void setTimeRange(double min, double max);
   AlertInfo alertInfo(double sec);
-  QPixmap thumbnail(double sec);
   void parseQLog(std::shared_ptr<LogReader> qlog);
 
   const double factor = 1000.0;
@@ -68,6 +63,7 @@ public:
   void paintGL() override;
   void showPausedOverlay() { fade_animation->start(); }
   float overlayOpacity() const { return overlay_opacity; }
+  void setAlert(const AlertInfo &alert);
   void setOverlayOpacity(float opacity) {
     overlay_opacity = opacity;
     update();
@@ -76,6 +72,7 @@ public:
 private:
   float overlay_opacity;
   QPropertyAnimation *fade_animation;
+  AlertInfo alert_;
 };
 
 class VideoWidget : public QFrame {
@@ -91,7 +88,6 @@ protected:
   void updatePlayBtnState();
   QWidget *createCameraWidget();
   QHBoxLayout *createPlaybackController();
-  void loopPlaybackClicked();
   void vipcAvailableStreamsUpdated(std::set<VisionStreamType> streams);
 
   StreamCameraView *cam_widget;
@@ -99,10 +95,8 @@ protected:
   ToolButton *seek_backward_btn = nullptr;
   ToolButton *play_btn = nullptr;
   ToolButton *seek_forward_btn = nullptr;
-  ToolButton *loop_btn = nullptr;
   QToolButton *speed_btn = nullptr;
   ToolButton *skip_to_end_btn = nullptr;
-  InfoLabel *alert_label = nullptr;
   Slider *slider = nullptr;
-  QTabBar *camera_tab = nullptr;
+  TabBar *camera_tab = nullptr;
 };
