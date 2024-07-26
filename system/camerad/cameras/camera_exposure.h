dbc7bc96d9e5b90097d9303acea92a50afc6bc28
deanlee <deanlee3@gmail.com>
2024-07-26 20:02:52 +0800

    dd

---
+++ system/camerad/cameras/camera_exposure.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <mutex>
+#include <vector>
+
+#include "common/params.h"
+#include "system/camerad/sensors/sensor.h"
+
+class CameraExposure {
+public:
+  CameraExposure(int camera_num, const SensorInfo *sensor_info, int width, int height, float focal_len);
+  void updateFrameMetaData(FrameMetadata&meta_data);
+  void updateScore(float desired_ev, int exp_t, int exp_g_idx, float exp_gain);
+  std::vector<i2c_random_wr_payload> getExposureRegisters(const CameraBuf &buf, int x_skip, int y_skip);
+
+private:
+  std::mutex exp_lock;
+
+  int exposure_time = 5;
+  bool dc_gain_enabled = false;
+  int dc_gain_weight = 0;
+  int gain_idx = 0;
+  float analog_gain_frac = 0;
+
+  float cur_ev[3] = {};
+  float best_ev_score = 0;
+  int new_exp_g = 0;
+  int new_exp_t = 0;
+
+  Rect ae_xywh = {};
+  float measured_grey_fraction = 0;
+  float target_grey_fraction = 0.3;
+  float fl_pix = 0;
+  Params params;
+  const SensorInfo *ci = nullptr;
+};
--- system/camerad/cameras/camera_qcom2.h
+++ system/camerad/cameras/camera_qcom2.h
@@ -5,9 +5,9 @@
 #include <vector>
 
 #include "system/camerad/cameras/camera_common.h"
+#include "system/camerad/cameras/camera_exposure.h"
 #include "system/camerad/cameras/camera_util.h"
 #include "system/camerad/sensors/sensor.h"
-#include "common/params.h"
 #include "common/util.h"
 
 #define FRAME_BUF_COUNT 4
@@ -46,35 +46,6 @@ const CameraConfig DRIVER_CAMERA_CONFIG = {
   .publish_name = "driverCameraState",
   .init_camera_state = &cereal::Event::Builder::initDriverCameraState,
   .enabled = !getenv("DISABLE_DRIVER"),
-};
-
-class CameraExposure {
-public:
-  CameraExposure(int camera_num, const SensorInfo *sensor_info, int width, int height, float focal_len);
-  void updateFrameMetaData(FrameMetadata&meta_data);
-  void updateScore(float desired_ev, int exp_t, int exp_g_idx, float exp_gain);
-  std::vector<i2c_random_wr_payload> getExposureRegisters(const CameraBuf &buf, int x_skip, int y_skip);
-
-private:
-  std::mutex exp_lock;
-
-  int exposure_time = 5;
-  bool dc_gain_enabled = false;
-  int dc_gain_weight = 0;
-  int gain_idx = 0;
-  float analog_gain_frac = 0;
-
-  float cur_ev[3] = {};
-  float best_ev_score = 0;
-  int new_exp_g = 0;
-  int new_exp_t = 0;
-
-  Rect ae_xywh = {};
-  float measured_grey_fraction = 0;
-  float target_grey_fraction = 0.3;
-  float fl_pix = 0;
-  Params params;
-  const SensorInfo *ci = nullptr;
 };
 
 class CameraState {
