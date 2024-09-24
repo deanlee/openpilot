b7acf9412c10036f6eed1e5629edb73f0c9275ea
deanlee <deanlee3@gmail.com>
2024-09-25 02:06:59 +0800

    dd

--- selfdrive/ui/qt/onroad/buttons.cc
+++ selfdrive/ui/qt/onroad/buttons.cc
@@ -4,13 +4,4 @@
 
 #include "selfdrive/ui/qt/util.h"
 
-void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity) {
-  p.setRenderHint(QPainter::Antialiasing);
-  p.setOpacity(1.0);  // bg dictates opacity of ellipse
-  p.setPen(Qt::NoPen);
-  p.setBrush(bg);
-  p.drawEllipse(center, btn_size / 2, btn_size / 2);
-  p.setOpacity(opacity);
-  p.drawPixmap(center - QPoint(img.width() / 2, img.height() / 2), img);
-  p.setOpacity(1.0);
-}
+
--- selfdrive/ui/qt/onroad/buttons.h
+++ selfdrive/ui/qt/onroad/buttons.h
@@ -1,6 +1,3 @@
 #pragma once
-
 const int btn_size = 192;
 const int img_size = (btn_size / 4) * 3;
-
-void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity);
