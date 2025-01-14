0d49813c9e34fb1e2b5968ed77dc2225095fae7c
deanlee <deanlee3@gmail.com>
2025-01-15 01:10:23 +0800

    dd

--- system/ui/raylib/controls/keyboard.cc
+++ system/ui/raylib/controls/keyboard.cc
@@ -45,21 +45,28 @@ std::vector<std::vector<std::string>> specials = {
 };
 
 Keyboard::Keyboard() {
+  GuiSetFont(pApp->getFont());
+  GuiSetStyle(DEFAULT, TEXT_SIZE, 40);
 }
 
 void Keyboard::render(const Rectangle &rect) {
   GuiTextBox((Rectangle){rect.x, rect.y, rect.width, 30}, inputText, 256, true);
 
   // Draw the keyboard below the input box
-  float keyWidth = rect.width / 10;
-  float keyHeight = 40;
+  // float keyWidth = rect.width / 10;
+  float keyHeight = 155;
+
+  // float h_space = 10;
+  float v_space = 10;
   int keyIndex = 0;
+
   for (int row = 0; row < lowercase.size(); row++) {
     const auto &keys = lowercase[row];
+    int start_x = row == 1 ? rect.x + 90 : rect.x;
+    float key_width = row == 1 ? (rect.width - 180) / keys.size() : (rect.width) / keys.size();
     for (int col = 0; col < keys.size(); col++) {
-      Rectangle keyRect = {rect.x + col * keyWidth, rect.y + 40 + row * keyHeight, keyWidth, keyHeight};
+      Rectangle keyRect = {start_x + col * key_width, rect.y + 40 + row * (keyHeight + v_space), key_width, keyHeight};
       if (GuiButton(keyRect, keys[col].c_str())) {
-        // Append the key to the input text
         int len = strlen(inputText);
         if (len < 255) {
           inputText[len] = keys[col][0];
