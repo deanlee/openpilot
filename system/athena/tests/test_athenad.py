a9bd6a8da803ebcc9579791399d372f384365bf0
deanlee <deanlee3@gmail.com>
2025-01-31 23:48:22 +0800

    dd

--- system/athena/tests/test_athenad.py
+++ system/athena/tests/test_athenad.py
@@ -76,7 +76,7 @@ class TestAthenadMethods:
       self.params.put(k, v)
     self.params.put_bool("GsmMetered", True)
 
-    athenad.upload_handler.clear()
+    athenad.upload_manager.clear()
 
     for i in os.listdir(Paths.log_root()):
       p = os.path.join(Paths.log_root(), i)
