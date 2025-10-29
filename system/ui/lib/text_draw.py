0be47d5a677f7626874c3d43ba43c26ffe0f364b
deanlee <deanlee3@gmail.com>
2025-10-29 21:18:17 +0800

    dd

--- system/ui/lib/text_draw.py
+++ system/ui/lib/text_draw.py
@@ -55,27 +55,3 @@ def draw_text(rect: rl.Rectangle, text: str, font_weight: FontWeight, font_size:
 
     rl.draw_text_ex(font, line, rl.Vector2(x, y), font_size, 0, color)
     y += line_height
-
-
-def draw_text_center(rect: rl.Rectangle, text: str, font: rl.Font, font_size: int, color: rl.Color):
-  """Draw text centered within the given rectangle."""
-  text_size = measure_text_cached(font, text, font_size)
-  text_x = rect.x + (rect.width - text_size.x) / 2
-  text_y = rect.y + (rect.height - text_size.y) / 2
-  rl.draw_text_ex(font, text, rl.Vector2(text_x, text_y), font_size, 0, color)
-
-
-def draw_text_right(rect: rl.Rectangle, text: str, font: rl.Font, font_size: int, color: rl.Color):
-  """Draw text right-aligned within the given rectangle."""
-  text_size = measure_text_cached(font, text, font_size)
-  text_x = rect.x + rect.width - text_size.x
-  text_y = rect.y + (rect.height - text_size.y) / 2
-  rl.draw_text_ex(font, text, rl.Vector2(text_x, text_y), font_size, 0, color)
-
-
-def draw_text_left(rect: rl.Rectangle, text: str, font: rl.Font, font_size: int, color: rl.Color):
-  """Draw text left-aligned within the given rectangle."""
-  text_size = measure_text_cached(font, text, font_size)
-  text_x = rect.x
-  text_y = rect.y + (rect.height - text_size.y) / 2
-  rl.draw_text_ex(font, text, rl.Vector2(text_x, text_y), font_size, 0, color)
