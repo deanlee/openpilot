1146ae2f581babb5bf40eafdc9a89c71dfea12c1
deanlee <deanlee3@gmail.com>
2025-11-21 18:39:48 +0800

    dd

--- system/ui/widgets/label.py
+++ system/ui/widgets/label.py
@@ -37,49 +37,88 @@ class RenderElement:
 
 
 class TextLayout:
-  def __init__(self):
-    self._text: str = ""
+  def __init__(self, text: str = "", font: rl.Font = None,
+               font_size: int = 16, max_width: int = 0,
+               text_color: rl.Color = rl.BLACK,
+               text_alignment: int = rl.GuiTextAlignment.TEXT_ALIGN_LEFT,
+               text_alignment_vertical: int = rl.GuiTextAlignmentVertical.TEXT_ALIGN_TOP,
+               icon: rl.Texture = None):
+    self._text: str = text
+    self._text_color: rl.Color = text_color
     self._width: int = 0
-    self._font: rl.Font | None = None
-    self._font_size: int = 0
+    self._font: rl.Font | None = font
+    self._font_size: int = font_size
     self._max_width: int = 0
     self._letter_spacing: float = 0.0
+    self._text_alignment: int = text_alignment
+    self._text_alignment_vertical: int = text_alignment_vertical
+    self._icon = icon
     self._elements: list[RenderElement] = []
     self._text_padding:int = 0
     self._elide_right: bool = False
     self._rect: rl.Rectangle = rl.Rectangle(0, 0, 0, 0)
 
-  def update(self, text: str, font: rl.Font, font_size: int, max_width: int, letter_spacing: float = 0.0):
+  def set_text(self, text: str):
+    if text != self.text:
+      self._text = text
+      self._elements.clear()
+
+  def set_font(self, font: rl.Font):
+    if font != self._font:
+      self._font = font
+      self._elements.clear()
+
+  def set_font_weight(self, font_weight: FontWeight):
+    font = gui_app.font(font_weight)
+    if font != self._font:
+      self._font = font
+      self._elements.clear()
+
+  def set_font_size(self, font_size: int):
+    if font_size != self._font_size:
+      self._font_size = font_size
+      self._elements.clear()
+
+  def set_text_color(self, color: rl.Color):
+    if color != self._text_color:
+      self._text_color = color
+
+  def set_rect(self, rect: rl.Rectangle):
+    if rect != self._rect:
+      self._rect = rect
+      self._elements.clear()
+
+  def update_layout(self):
     self._elements.clear()
 
-    if not text and not self._icon:
+    if not self._text and not self._icon:
       return
 
     content_width = self._rect.width - self._text_padding * 2
 
     if self._elide_right:
-      text_size = measure_text_cached(self._font, text, self._font_size)
+      text_size = measure_text_cached(self._font, self._text, self._font_size)
       content_width = self._rect.width - self._text_padding * 2
       if self._icon:
         content_width -= self._icon.width + ICON_PADDING
       if text_size.x > content_width:
         ellipsis = "..."
-        left, right = 0, len(text)
+        left, right = 0, len(self._text)
         while left < right:
           mid = (left + right) // 2
-          candidate = text[:mid] + ellipsis
+          candidate = self._text[:mid] + ellipsis
           candidate_size = measure_text_cached(self._font, candidate, self._font_size)
           if candidate_size.x <= content_width:
             left = mid + 1
           else:
             right = mid
-        display_text = text[: left - 1] + ellipsis if left > 0 else ellipsis
+        display_text = self._text[: left - 1] + ellipsis if left > 0 else ellipsis
       else:
-        display_text = text
+        display_text = self._text
 
       wrapped_lines = [display_text] if display_text else []
     else:
-      wrapped_lines = wrap_text(self._font, text, self._font_size, int(content_width)) if text else []
+      wrapped_lines = wrap_text(self._font, self._text, self._font_size, int(content_width)) if self._text else []
 
     # Calculate starting Y position (relative to rect origin)
     if wrapped_lines:
@@ -165,10 +204,14 @@ class TextLayout:
       element.x += x_base
 
   def render(self, rect: rl.Rectangle):
-    # Apply absolute position from self._rect when rendering
+    self.set_rect(rect)
+    if not self._elements:
+      self.update_layout()
+
+    # Apply absolute position from rect when rendering
     for element in self._elements:
-      tex = element.texture
       pos = rl.Vector2(rect.x + element.x, rect.y + element.y)
+      tex = element.texture
       if tex is None:
         rl.draw_text_ex(self._font, element.text, pos, self._font_size, 0, self._text_color)
       elif tex is self._icon:
@@ -430,116 +473,30 @@ class Label(Widget):
                ):
 
     super().__init__()
-    self._font_weight = font_weight
-    self._font = gui_app.font(self._font_weight)
-    self._font_size = font_size
-    self._text_alignment = text_alignment
-    self._text_alignment_vertical = text_alignment_vertical
     self._text_padding = text_padding
-    self._text_color = text_color
-    self._icon = icon
     self._elide_right = elide_right
     self._line_scale = line_scale
-
     self._text = text
-    self.set_text(text)
+    self._text_layout = TextLayout(_resolve_value(text),
+                                   font=gui_app.font(font_weight),
+                                   font_size=font_size,
+                                   text_color=text_color,
+                                   text_alignment=text_alignment,
+                                   text_alignment_vertical=text_alignment_vertical,
+                                   icon=icon)
 
   def set_text(self, text):
     self._text = text
-    self._update_text(self._text)
+    self._text_layout.set_text(_resolve_value(text))
 
   def set_text_color(self, color):
-    self._text_color = color
+    self._text_layout.set_text_color(color)
 
   def set_font_size(self, size):
-    self._font_size = size
-    self._update_text(self._text)
+    self._text_layout.set_font_size(size)
 
-  def _update_text(self, text):
-    self._emojis = []
-    self._text_size = []
-    text = _resolve_value(text)
-
-    if self._elide_right:
-      display_text = text
-
-      # Elide text to fit within the rectangle
-      text_size = measure_text_cached(self._font, text, self._font_size)
-      content_width = self._rect.width - self._text_padding * 2
-      if self._icon:
-        content_width -= self._icon.width + ICON_PADDING
-      if text_size.x > content_width:
-        _ellipsis = "..."
-        left, right = 0, len(text)
-        while left < right:
-          mid = (left + right) // 2
-          candidate = text[:mid] + _ellipsis
-          candidate_size = measure_text_cached(self._font, candidate, self._font_size)
-          if candidate_size.x <= content_width:
-            left = mid + 1
-          else:
-            right = mid
-        display_text = text[: left - 1] + _ellipsis if left > 0 else _ellipsis
-
-      self._text_wrapped = [display_text]
-    else:
-      self._text_wrapped = wrap_text(self._font, text, self._font_size, round(self._rect.width - (self._text_padding * 2)))
-
-    for t in self._text_wrapped:
-      self._emojis.append(find_emoji(t))
-      self._text_size.append(measure_text_cached(self._font, t, self._font_size))
-
-  def _render(self, _):
-    # Text can be a callable
-    # TODO: cache until text changed
-    self._update_text(self._text)
-
-    text_size = self._text_size[0] if self._text_size else rl.Vector2(0.0, 0.0)
-    if self._text_alignment_vertical == rl.GuiTextAlignmentVertical.TEXT_ALIGN_MIDDLE:
-      total_text_height = sum(ts.y for ts in self._text_size) or self._font_size * FONT_SCALE
-      text_pos = rl.Vector2(self._rect.x, (self._rect.y + (self._rect.height - total_text_height) // 2))
-    else:
-      text_pos = rl.Vector2(self._rect.x, self._rect.y)
-
-    if self._icon:
-      icon_y = self._rect.y + (self._rect.height - self._icon.height) / 2
-      if len(self._text_wrapped) > 0:
-        if self._text_alignment == rl.GuiTextAlignment.TEXT_ALIGN_LEFT:
-          icon_x = self._rect.x + self._text_padding
-          text_pos.x = self._icon.width + ICON_PADDING
-        elif self._text_alignment == rl.GuiTextAlignment.TEXT_ALIGN_CENTER:
-          total_width = self._icon.width + ICON_PADDING + text_size.x
-          icon_x = self._rect.x + (self._rect.width - total_width) / 2
-          text_pos.x = self._icon.width + ICON_PADDING
-        else:
-          icon_x = (self._rect.x + self._rect.width - text_size.x - self._text_padding) - ICON_PADDING - self._icon.width
-      else:
-        icon_x = self._rect.x + (self._rect.width - self._icon.width) / 2
-      rl.draw_texture_v(self._icon, rl.Vector2(icon_x, icon_y), rl.WHITE)
-
-    for text, text_size, emojis in zip_longest(self._text_wrapped, self._text_size, self._emojis, fillvalue=[]):
-      line_pos = rl.Vector2(text_pos.x, text_pos.y)
-      if self._text_alignment == rl.GuiTextAlignment.TEXT_ALIGN_LEFT:
-        line_pos.x += self._text_padding
-      elif self._text_alignment == rl.GuiTextAlignment.TEXT_ALIGN_CENTER:
-        line_pos.x += (self._rect.width - text_size.x) // 2
-      elif self._text_alignment == rl.GuiTextAlignment.TEXT_ALIGN_RIGHT:
-        line_pos.x += self._rect.width - text_size.x - self._text_padding
-
-      prev_index = 0
-      for start, end, emoji in emojis:
-        text_before = text[prev_index:start]
-        width_before = measure_text_cached(self._font, text_before, self._font_size)
-        rl.draw_text_ex(self._font, text_before, line_pos, self._font_size, 0, self._text_color)
-        line_pos.x += width_before.x
-
-        tex = emoji_tex(emoji)
-        rl.draw_texture_ex(tex, line_pos, 0.0, self._font_size / tex.height * FONT_SCALE, self._text_color)
-        line_pos.x += self._font_size * FONT_SCALE
-        prev_index = end
-      rl.draw_text_ex(self._font, text[prev_index:], line_pos, self._font_size, 0, self._text_color)
-      text_pos.y += (text_size.y or self._font_size * FONT_SCALE) * self._line_scale
-
+  def _render(self, rect: rl.Rectangle):
+    self._text_layout.render(rect)
 
 class UnifiedLabel(Widget):
   """
