b1ce6aab441fce31a617efda61ba4b9ed09650cb
Dean Lee <deanlee3@gmail.com>
2025-12-23 23:03:19 +0800

    simplify home layout (#29)

--- selfdrive/ui/layouts/home.py
+++ selfdrive/ui/layouts/home.py
@@ -32,6 +32,9 @@ class HomeLayout(Widget):
     super().__init__()
     self.params = Params()
 
+    self._prime_widget = PrimeWidget()
+    self._setup_widget = SetupWidget()
+    self._exp_mode_button = ExperimentalModeButton()
     self.update_alert = UpdateAlert()
     self.offroad_alert = OffroadAlert()
 
@@ -47,23 +50,10 @@ class HomeLayout(Widget):
     self._prev_update_available = False
     self._prev_alerts_present = False
 
-    self.header_rect = rl.Rectangle(0, 0, 0, 0)
-    self.content_rect = rl.Rectangle(0, 0, 0, 0)
-    self.left_column_rect = rl.Rectangle(0, 0, 0, 0)
-    self.right_column_rect = rl.Rectangle(0, 0, 0, 0)
-
-    self.update_notif_rect = rl.Rectangle(0, 0, 200, HEADER_HEIGHT - 10)
-    self.alert_notif_rect = rl.Rectangle(0, 0, 220, HEADER_HEIGHT - 10)
-
-    self._prime_widget = PrimeWidget()
-    self._setup_widget = SetupWidget()
-
-    self._exp_mode_button = ExperimentalModeButton()
     self._setup_callbacks()
 
   def show_event(self):
     self._exp_mode_button.show_event()
-    self.last_refresh = time.monotonic()
     self._refresh()
 
   def _setup_callbacks(self):
@@ -91,85 +81,61 @@ class HomeLayout(Widget):
     current_time = time.monotonic()
     if current_time - self.last_refresh >= REFRESH_INTERVAL:
       self._refresh()
-      self.last_refresh = current_time
 
     self._render_header()
 
     # Render content based on current state
     if self.current_state == HomeLayoutState.HOME:
-      self._render_home_content()
+      self._render_left_column()
+      self._render_right_column()
     elif self.current_state == HomeLayoutState.UPDATE:
-      self._render_update_view()
+      self.update_alert.render(self.content_rect)
     elif self.current_state == HomeLayoutState.ALERTS:
-      self._render_alerts_view()
+      self.offroad_alert.render(self.content_rect)
 
   def _update_state(self):
-    self.header_rect = rl.Rectangle(
-      self._rect.x + CONTENT_MARGIN, self._rect.y + CONTENT_MARGIN, self._rect.width - 2 * CONTENT_MARGIN, HEADER_HEIGHT
-    )
+    self.header_rect = rl.Rectangle(self._rect.x + CONTENT_MARGIN, self._rect.y + CONTENT_MARGIN,
+                                    self._rect.width - 2 * CONTENT_MARGIN, HEADER_HEIGHT)
+    self.content_rect = rl.Rectangle(self.header_rect.x, self.header_rect.y + HEADER_HEIGHT + SPACING,
+                                     self.header_rect.width, self._rect.height - HEADER_HEIGHT - SPACING - 2*CONTENT_MARGIN)
 
-    content_y = self._rect.y + CONTENT_MARGIN + HEADER_HEIGHT + SPACING
-    content_height = self._rect.height - CONTENT_MARGIN - HEADER_HEIGHT - SPACING - CONTENT_MARGIN
+    l_width = self.content_rect.width - RIGHT_COLUMN_WIDTH - SPACING
+    self.left_col_rect = rl.Rectangle(self.content_rect.x, self.content_rect.y, l_width, self.content_rect.height)
+    self.right_col_rect = rl.Rectangle(self.content_rect.x + l_width + SPACING, self.content_rect.y,
+                                          RIGHT_COLUMN_WIDTH, self.content_rect.height)
 
-    self.content_rect = rl.Rectangle(
-      self._rect.x + CONTENT_MARGIN, content_y, self._rect.width - 2 * CONTENT_MARGIN, content_height
-    )
-
-    left_width = self.content_rect.width - RIGHT_COLUMN_WIDTH - SPACING
-
-    self.left_column_rect = rl.Rectangle(self.content_rect.x, self.content_rect.y, left_width, self.content_rect.height)
-
-    self.right_column_rect = rl.Rectangle(
-      self.content_rect.x + left_width + SPACING, self.content_rect.y, RIGHT_COLUMN_WIDTH, self.content_rect.height
-    )
-
-    self.update_notif_rect.x = self.header_rect.x
-    self.update_notif_rect.y = self.header_rect.y + (self.header_rect.height - 60) // 2
-
-    notif_x = self.header_rect.x + (220 if self.update_available else 0)
-    self.alert_notif_rect.x = notif_x
-    self.alert_notif_rect.y = self.header_rect.y + (self.header_rect.height - 60) // 2
+    self.update_notif_rect = rl.Rectangle(self.header_rect.x, self.header_rect.y + 10, 200, 60)
+    self.alert_notif_rect = rl.Rectangle(self.header_rect.x + (220 if self.update_available else 0),
+                                         self.header_rect.y + 10, 220, 60)
 
   def _handle_mouse_release(self, mouse_pos: MousePos):
-    super()._handle_mouse_release(mouse_pos)
-
     if self.update_available and rl.check_collision_point_rec(mouse_pos, self.update_notif_rect):
       self._set_state(HomeLayoutState.UPDATE)
     elif self.alert_count > 0 and rl.check_collision_point_rec(mouse_pos, self.alert_notif_rect):
       self._set_state(HomeLayoutState.ALERTS)
 
-  def _render_header(self):
+  def _draw_badge(self, rect, text, color, active_color, is_active):
+    rl.draw_rectangle_rounded(rect, 0.3, 10, active_color if is_active else color)
     font = gui_app.font(FontWeight.MEDIUM)
+    sz = measure_text_cached(font, text, HEAD_BUTTON_FONT_SIZE)
+    rl.draw_text_ex(font, text, rl.Vector2(rect.x + (rect.width - sz.x)/2, rect.y + (rect.height - sz.y)/2),
+                    HEAD_BUTTON_FONT_SIZE, 0, rl.WHITE)
 
+  def _render_header(self):
     version_text_width = self.header_rect.width
 
     # Update notification button
     if self.update_available:
       version_text_width -= self.update_notif_rect.width
-
-      # Highlight if currently viewing updates
-      highlight_color = rl.Color(75, 95, 255, 255) if self.current_state == HomeLayoutState.UPDATE else rl.Color(54, 77, 239, 255)
-      rl.draw_rectangle_rounded(self.update_notif_rect, 0.3, 10, highlight_color)
-
-      text = tr("UPDATE")
-      text_size = measure_text_cached(font, text, HEAD_BUTTON_FONT_SIZE)
-      text_x = self.update_notif_rect.x + (self.update_notif_rect.width - text_size.x) // 2
-      text_y = self.update_notif_rect.y + (self.update_notif_rect.height - text_size.y) // 2
-      rl.draw_text_ex(font, text, rl.Vector2(int(text_x), int(text_y)), HEAD_BUTTON_FONT_SIZE, 0, rl.WHITE)
+      self._draw_badge(self.update_notif_rect, tr("UPDATE"), rl.Color(54, 77, 239, 255),
+                       rl.Color(75, 95, 255, 255), self.current_state == HomeLayoutState.UPDATE)
 
     # Alert notification button
     if self.alert_count > 0:
       version_text_width -= self.alert_notif_rect.width
-
-      # Highlight if currently viewing alerts
-      highlight_color = rl.Color(255, 70, 70, 255) if self.current_state == HomeLayoutState.ALERTS else rl.Color(226, 44, 44, 255)
-      rl.draw_rectangle_rounded(self.alert_notif_rect, 0.3, 10, highlight_color)
-
-      alert_text = trn("{} ALERT", "{} ALERTS", self.alert_count).format(self.alert_count)
-      text_size = measure_text_cached(font, alert_text, HEAD_BUTTON_FONT_SIZE)
-      text_x = self.alert_notif_rect.x + (self.alert_notif_rect.width - text_size.x) // 2
-      text_y = self.alert_notif_rect.y + (self.alert_notif_rect.height - text_size.y) // 2
-      rl.draw_text_ex(font, alert_text, rl.Vector2(int(text_x), int(text_y)), HEAD_BUTTON_FONT_SIZE, 0, rl.WHITE)
+      txt = trn("{} ALERT", "{} ALERTS", self.alert_count).format(self.alert_count)
+      self._draw_badge(self.alert_notif_rect, txt, rl.Color(226, 44, 44, 255),
+                       rl.Color(255, 70, 70, 255), self.current_state == HomeLayoutState.ALERTS)
 
     # Version text (right aligned)
     if self.update_available or self.alert_count > 0:
@@ -177,37 +143,27 @@ class HomeLayout(Widget):
 
     version_rect = rl.Rectangle(self.header_rect.x + self.header_rect.width - version_text_width, self.header_rect.y,
                                 version_text_width, self.header_rect.height)
-    gui_label(version_rect, self._version_text, 48, rl.WHITE, align=Align.RIGHT)
-
-  def _render_home_content(self):
-    self._render_left_column()
-    self._render_right_column()
-
-  def _render_update_view(self):
-    self.update_alert.render(self.content_rect)
-
-  def _render_alerts_view(self):
-    self.offroad_alert.render(self.content_rect)
+    gui_label(version_rect, self._version_text, 48, rl.WHITE, align=Align.RIGHT, elide_right=True)
 
   def _render_left_column(self):
-    self._prime_widget.render(self.left_column_rect)
+    self._prime_widget.render(self.left_col_rect)
 
   def _render_right_column(self):
     exp_height = 125
-    exp_rect = rl.Rectangle(
-      self.right_column_rect.x, self.right_column_rect.y, self.right_column_rect.width, exp_height
-    )
+    exp_rect = rl.Rectangle(self.right_col_rect.x, self.right_col_rect.y, self.right_col_rect.width, exp_height)
     self._exp_mode_button.render(exp_rect)
 
     setup_rect = rl.Rectangle(
-      self.right_column_rect.x,
-      self.right_column_rect.y + exp_height + SPACING,
-      self.right_column_rect.width,
-      self.right_column_rect.height - exp_height - SPACING,
+      self.right_col_rect.x,
+      self.right_col_rect.y + exp_height + SPACING,
+      self.right_col_rect.width,
+      self.right_col_rect.height - exp_height - SPACING,
     )
     self._setup_widget.render(setup_rect)
 
   def _refresh(self):
+    self.last_refresh = time.monotonic()
+
     self._version_text = self._get_version_text()
     update_available = self.update_alert.refresh()
     alert_count = self.offroad_alert.refresh()
