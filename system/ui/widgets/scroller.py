f4719e13293c2cae554bbea39d2215d59fa94bbb
deanlee <deanlee3@gmail.com>
2025-12-25 18:31:52 +0800

    simplify get_scroll

--- system/ui/widgets/scroller.py
+++ system/ui/widgets/scroller.py
@@ -133,34 +133,16 @@ class Scroller(Widget):
 
     # Snap closest item to center
     center_pos = self._rect.x + self._rect.width / 2 if self._horizontal else self._rect.y + self._rect.height / 2
-    closest_delta_pos = float('inf')
-    scroll_snap_idx: int | None = None
-    for idx, item in enumerate(visible_items):
-      if self._horizontal:
-        delta_pos = (item.rect.x + item.rect.width / 2) - center_pos
-      else:
-        delta_pos = (item.rect.y + item.rect.height / 2) - center_pos
-      if abs(delta_pos) < abs(closest_delta_pos):
-        closest_delta_pos = delta_pos
-        scroll_snap_idx = idx
+    deltas = (((i.rect.x + i.rect.width / 2) if self._horizontal else (i.rect.y + i.rect.height / 2)) - center_pos
+              for i in visible_items)
+    closest_delta = min(deltas, key=abs, default=0)
 
-    if scroll_snap_idx is not None:
-      snap_item = visible_items[scroll_snap_idx]
-      if self.is_pressed:
-        # no snapping until released
-        self._scroll_snap_filter.x = 0
-      else:
-        # TODO: this doesn't handle two small buttons at the edges well
-        if self._horizontal:
-          snap_delta_pos = (center_pos - (snap_item.rect.x + snap_item.rect.width / 2)) / 10
-          snap_delta_pos = min(snap_delta_pos, -self.scroll_panel.get_offset() / 10)
-          snap_delta_pos = max(snap_delta_pos, (self._rect.width - self.scroll_panel.get_offset() - content_size) / 10)
-        else:
-          snap_delta_pos = (center_pos - (snap_item.rect.y + snap_item.rect.height / 2)) / 10
-          snap_delta_pos = min(snap_delta_pos, -self.scroll_panel.get_offset() / 10)
-          snap_delta_pos = max(snap_delta_pos, (self._rect.height - self.scroll_panel.get_offset() - content_size) / 10)
-        self._scroll_snap_filter.update(snap_delta_pos)
-
+    if self.is_pressed:
+      # no snapping until released
+      self._scroll_snap_filter.x = 0
+    else:
+      # TODO: this doesn't handle two small buttons at the edges well
+      self._scroll_snap_filter.update(-closest_delta / 10)
       self.scroll_panel.set_offset(self.scroll_panel.get_offset() + self._scroll_snap_filter.x)
 
     return self.scroll_panel.get_offset()
